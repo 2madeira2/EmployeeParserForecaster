602e9e0d5c41a9ca2c8e45f3cd160dbeb1456102
Dmitry <dmitridmitri04052000@gmail.com>
2022-04-21 22:48:56 +0300

    Beta ver. Added method that show good transfers between departments

--- src/main/java/EmployeesInfoParser.java
+++ src/main/java/EmployeesInfoParser.java
@@ -1,22 +1,18 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class EmployeesInfoParser {
+
     public static void parseEmployeesInfo(String path) {
         try {
             List<String> lines = Files.readAllLines(Paths.get(path));
-            Map<String, List<String>> employeesGroupingByDepartment = lines
-                    .stream()
-                    .map(s -> s.split(" "))
-                    .collect(Collectors.groupingBy(s -> s[2], Collectors.mapping(s -> s[0] + " " + s[1], Collectors.toList())));
-            Map<String, Double> averageSalaryByDepartment = lines
-                    .stream()
-                    .map(s -> s.split(" "))
-                    .collect(Collectors.groupingBy(s -> s[2], Collectors.averagingInt(s -> Integer.parseInt(s[1]))));
+            Map<String, List<String>> employeesGroupingByDepartment = getEmployeesGroupingByDepartment(lines);
+            Map<String, Double> averageSalaryByDepartment = getAverageSalaryByDepartments(lines);
             for(Map.Entry<String, Double> e : averageSalaryByDepartment.entrySet()) {
                 System.out.println(e.getKey()
                         + " department. Average salary: "
@@ -28,7 +24,60 @@ public class EmployeesInfoParser {
         }
     }
 
+
+    private static Map<String, List<String>> getEmployeesGroupingByDepartment (List<String> list) {
+        return list.stream()
+                .map(s -> s.split(" "))
+                .collect(Collectors.groupingBy(s -> s[2], Collectors.mapping(s -> s[0] + " " + s[1], Collectors.toList())));
+    }
+
+    private static Map<String, Double> getAverageSalaryByDepartments (List<String> list) {
+        return list.stream()
+                    .map(s -> s.split(" "))
+                    .collect(Collectors.groupingBy(s -> s[2], Collectors.averagingInt(s -> Integer.parseInt(s[1]))));
+    }
+
+
+    private static Double getAverageSalaryByDepartment(List<String> list) {
+        if(list.isEmpty()) return 0d;
+        return list.stream()
+                .mapToInt(s -> Integer.parseInt(s.split(" ")[1]))
+                .average()
+                .getAsDouble();
+    }
+
+    private static List<String> getDepartmentsId(List<String> list) {
+        return list.stream()
+                .map(s -> s.split(" ")[2])
+                .distinct()
+                .collect(Collectors.toList());
+    }
+    private static void prognozeAllGoodWaysToTransferInDepartments(String path) {
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(path));
+            Map<String, List<String>> employeesGroupingByDepartment = getEmployeesGroupingByDepartment(lines);
+            for(Map.Entry<String, List<String>> e : employeesGroupingByDepartment.entrySet()) {
+                for(String worker : e.getValue()) {
+                    for (String departmentId : getDepartmentsId(lines)) {
+                        List<String> listWithAddingEmployee = new ArrayList<>(employeesGroupingByDepartment.get(departmentId));
+                        List<String> listWithRemovingEmployee = new ArrayList<>(employeesGroupingByDepartment.get(e.getKey()));
+                        listWithRemovingEmployee.remove(worker);
+                        listWithAddingEmployee.add(worker);
+                        if (getAverageSalaryByDepartment(employeesGroupingByDepartment.get(departmentId)) < getAverageSalaryByDepartment(listWithAddingEmployee)
+                        && getAverageSalaryByDepartment(employeesGroupingByDepartment.get(e.getKey())) < getAverageSalaryByDepartment(listWithRemovingEmployee)) {
+                            System.out.println(worker + " from " + e.getKey() + " department to " + departmentId + " department - is Good Thing");
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public static void main(String[] args) {
         parseEmployeesInfo("src/main/java/test.txt");
+        prognozeAllGoodWaysToTransferInDepartments("src/main/java/test.txt");
+
     }
 }
